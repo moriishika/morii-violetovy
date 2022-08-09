af5d3ce94fa1652df32b61f3340573aa220060e9
Ramanavami <48641207+moriishika@users.noreply.github.com>
2022-08-09 12:09:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+from keep_alive import keep_alive
+from discord.ext import commands
+
+bot = commands.Bot(
+	command_prefix="!",  # Change to desired prefix
+	case_insensitive=True  # Commands aren't case-sensitive
+)
+
+bot.author_id = 364596107524702208  # Change to your discord id!!!
+
+@bot.event 
+async def on_ready():  # When the bot is ready
+    print("I'm in")
+    print(bot.user)  # Prints the bot's username and identifier
+
+@bot.event
+async def on_message(message):
+  if message.content.startswith("$helloviole"):
+    await message.channel.send('Hello PAPA!')
+  if(message.content.startswith("$sayhi")):
+    await message.channel.send("Hello Mr. " + message.author.name)
+  
+
+extensions = [
+	'cogs.cog_example'  # Same name as it would be if you were importing it
+]
+
+if __name__ == '__main__':  # Ensures this is the file being ran
+	for extension in extensions:
+		bot.load_extension(extension)  # Loades every extension.
+
+keep_alive()  # Starts a webserver to be pinged.
+token = os.environ.get("DISCORD_BOT_SECRET") 
+bot.run(token)  # Starts the bot
